5b2f9cfcda4cd8f976eb9c090e0106839a83b2ae
TReddy1-REISys <treddy@reisystems.com>
2020-10-21 17:26:14 -0400

    Changed to DB name

---
+++ target/classes/db/migration/V3__feedback.sql
@@ -0,0 +1,6 @@
+﻿set search_path = feedbackschema;
+
+INSERT INTO feedback(question_id,feedback_path,feedback_response,created_by,created_date,last_modified_by,last_modified_date)
+	values (1,'/alerts','{"type" : "select", "selected" : ["like"]}','ADMIN',now(),'ADMIN',now());
+INSERT INTO feedback(question_id,user_id,feedback_path,feedback_response,created_by,created_date,last_modified_by,last_modified_date) 
+	values (2,'nithin@gsa.gov','/access','{"type" : "multiselect", "selected" : ["FH"]}','ADMIN',now(),'ADMIN',now());
