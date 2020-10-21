d6e1ffb89f5fd6f09679e67b85b9ae947fc5424b
TReddy1-REISys <treddy@reisystems.com>
2020-10-21 17:36:21 -0400

    Feedback service from GSA IAE repository https://github.helix.gsa.gov/GSA-IAE-APPS/feedback-service.git

--- target/classes/db/migration/V2__questions.sql
+++
@@ -1,8 +0,0 @@
-set search_path = feedbackschema;
-
-insert into question(question_desc,question_options,created_by,created_date,last_modified_by,last_modified_date)
-	values ('What do you like or dislike about beta.SAM.gov?', '{"type" : "textarea" , "options" : []}','ADMIN',now(), 'ADMIN',now());
-insert into question(question_desc,question_options,created_by,created_date,last_modified_by,last_modified_date)
-	values ('What changes or improvements would you suggest?', '{"type" : "textarea" , "options" : []}','ADMIN',now(), 'ADMIN',now());
-insert into question(question_desc,question_options,created_by,created_date,last_modified_by,last_modified_date)
-	values ('May we contact you if we have questions about your feedback?', '{"type" : "radio-text" , "options" : ["yes","no"]}','ADMIN',now(), 'ADMIN',now()); 
--- target/classes/db/migration/new/V5__questions_2.sql
+++
@@ -1,9 +0,0 @@
-set search_path = feedbackschema;
-
-UPDATE question SET status='inactive';
-
-insert into feedbackschema.question(question_desc,question_options,created_by,created_date,last_modified_by,last_modified_date, status)
-values(
-    'We welcome you to provide feedback on this site. If you have a technical issue or need a question answered, please go to the Federal Service Desk. Thank you.',
-    '{"type" : "textarea" , "options" : []}','ADMIN',now(), 'ADMIN',now(), 'active'
-    );
